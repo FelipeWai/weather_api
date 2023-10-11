45a8f28b78c52e18459b02020a09c3fa690a223a
FelipeWai <97367113+FelipeWai@users.noreply.github.com>
2023-10-11 01:32:51 -0300

    Weather api

--- app.py
+++ app.py
@@ -1,19 +1,50 @@
 import requests
 from pprint import pprint
 from api_key import api_key
+from datetime import datetime
 
-# city = input("Dogite a cidade: ")
+class CidadeInvalidaError(Exception):
+    pass
 
 base_url = "http://api.weatherapi.com/v1/current.json?"
 
+
+cidade = input("Digite a cidade: ")
+
 param = {
     "key": api_key,
-    "q": 'Santos',
-    "days": '6',
+    "q": cidade,
     "lang": "pt"
 }
 
-response = requests.get(base_url, params=param)
+try:
 
-pprint(response.json())
+    response = requests.get(base_url, params=param)
+    informacoes_da_cidade = response.json()
 
+
+    informacoes_na_tela_do_usuario = {
+        'Temp': informacoes_da_cidade['current']['temp_c'],
+        'vel_vento': informacoes_da_cidade['current']['wind_kph'],
+        'nome': informacoes_da_cidade['location']['name'],
+        'estado': informacoes_da_cidade['location']['region'],
+        'country': informacoes_da_cidade['location']['country'],
+        'horario': datetime.fromtimestamp(informacoes_da_cidade['location']['localtime_epoch']).strftime('%H:%M:%S'),
+    }
+        
+    if response.status_code == 400:
+        raise CidadeInvalidaError ("Type a valid city!")
+
+
+
+    print(f"""
+            Nome: {informacoes_na_tela_do_usuario['nome']}
+            Estado: {informacoes_na_tela_do_usuario['estado']}
+            country: {informacoes_na_tela_do_usuario['country']}
+            Velocidade do Vento: {informacoes_na_tela_do_usuario['vel_vento']}
+            Sensação Térmica: {informacoes_na_tela_do_usuario['Temp']}
+            horario: {informacoes_na_tela_do_usuario['horario']}""")
+
+
+except KeyError as e:
+    print(f"Erro: {e}")
